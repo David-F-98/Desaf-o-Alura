8371f1506d43273acf3c416199c20510d8b10198
David Nieto <u2903149@unimilitar.edu.co>
2023-02-02 16:38:35 -0500

    ejecucion.V2

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function encriptar(){
-    alert("hola");
+const textArea = document.querySelector('.text-area');
+const mensaje = document.querySelector('.mensaje');
+const copiar = document.querySelector('.copiar');
+
+function encriptar(textAreaN){
+    let matrizAceptada = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    for(i=0;i<matrizAceptada.length;i++){
+        if(textAreaN.includes(matrizAceptada[i][0])){
+            textAreaN = textAreaN.replaceAll(matrizAceptada[i][0],matrizAceptada[i][1]);
+        }
+    }
+    return textAreaN;
 }
 
-function desencriptar(){
-    alert("hola2");
+
+function desencriptar(mensajeCod){
+    let matrizAceptada = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    for(i=0;i<matrizAceptada.length;i++){
+        if(mensajeCod.includes(matrizAceptada[i][1])){
+            mensajeCod = mensajeCod.replaceAll(matrizAceptada[i][1],matrizAceptada[i][0]);
+        }
+    }
+    return mensajeCod;
+}
+
+function btnEncriptar(){
+    mensaje.value = encriptar(textArea);
+}
+
+function btnDesencriptar(){
+
 }
 
 function copiar(){
-    alert("hola2");
+    
 }
